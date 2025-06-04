350a974fe51c5a552c1933ce41ca75411a035b84
Avesta <92775375+Avestair@users.noreply.github.com>
2025-06-05 00:16:44 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function DashBoard() {
   return (
     <div className="grid gap-6">
       <h1 className="text-xl font-semibold md:text-2xl">خوش آمدید!</h1>
-      <div className="flex gap-5">
+      <div className="md:flex grid gap-2.5 px-2 md:gap-5">
         <Link href={"/users"}>
-          <Card bodyClassName="grid gap-4 w-[200px]">
+          <Card bodyClassName="grid gap-4 md:w-[200px] w-full">
             <PiUserListDuotone className="size-6" />
             <p className="justify-self-end">کاربران</p>
           </Card>
         </Link>
 
         <Link href={"/searchuser"}>
-          <Card bodyClassName="grid gap-4 w-[200px]">
+          <Card bodyClassName="grid gap-4 md:w-[200px] w-full">
             <PiUserDuotone className="size-6" />
             <p className="justify-self-end">جست و جوی کاربر</p>
           </Card>
